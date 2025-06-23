dbeeb1293242e40d7c26137205055d2342d6d642
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2025-06-23 18:05:59 +0530

    add a function to get shortest distance between two position

--- main.js
+++ main.js
@@ -19,4 +19,40 @@ function move([x, y]) {
   });
 }
 
+function knightMoves(start, [x, y]) {
+  let queue = [];
+  queue.push({
+    current: start,
+    path: [start],
+  });
 
+  const visitedSquares = new Set();
+
+  while (queue.length > 0) {
+    const currPosition = queue[0].current;
+    const currPath = queue[0].path;
+
+    if (currPosition[0] === x && currPosition[1] === y) {
+      return currPath;
+    }
+
+    let possibleMoves = move(currPosition);
+
+    for (let move of possibleMoves) {
+
+        // check if the square is already exist as visited
+
+      if (!visitedSquares.has(JSON.stringify(move))) {
+        queue.push({
+          current: move,
+          path: currPath.concat([move]),
+        });
+      }
+    }
+
+    // I stringify array since array create reference and always give false if we compare them.
+
+    visitedSquares.add(JSON.stringify(currPosition));
+    queue.shift();
+  }
+}
